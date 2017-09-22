65309eabdda8f69e84aea49746d83917c73ed787
dicoju <julien.culieras@gmail.com>
2017-09-22 11:48:28 +0200

    first commit

---
+++ mvc_maison/languages/FR-fr.php
@@ -0,0 +1,13 @@
+<?php
+//isoler ici dans des constantes les textes affichés sur le site
+define('LOGO', 'Logo de la compagnie'); // Affiché si image non trouvée
+
+define('MENU_ACCUEIL','Accueil');
+
+define('TEXTE_PAGE_404','Oops, la page demandée n\'existe pas !');
+define('MESSAGE_ERREUR',"Une erreur s'est produite");
+
+define('TITRE', '');
+
+define('TITRE_PAGE_ACCUEIL_TOUS','Toutes les photos');
+
