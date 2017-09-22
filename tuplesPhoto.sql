17fb3026a8b57388f7bdf2d2f0ed95c037a4b9e2
hugo <hugo.lager01@gmail.com>
2017-09-22 11:57:38 +0200

    test bidon

--- tuplesPhoto.sql
+++ tuplesPhoto.sql
@@ -1,4 +1,4 @@
-INSERT INTO Categorie VALUES (1, 'Animaux');
+﻿INSERT INTO Categorie VALUES (1, 'Animaux');
 INSERT INTO Categorie VALUES (2, 'Repas');
 INSERT INTO Categorie VALUES (3, 'Monuments');
 INSERT INTO Photo VALUES (1, 'DSC00011.jpg', 'Une perruche en cage', 1);
@@ -10,3 +10,4 @@ INSERT INTO Photo VALUES (6, 'DSC01280.jpg', 'Quelque chose de sculpté', 3);
 INSERT INTO Photo VALUES (7, 'DSC01436.jpg', 'Un monument lointain', 3);
 INSERT INTO Photo VALUES (8, 'DSC01464.jpg', 'Un monument très très loin', 3);
 INSERT INTO Photo VALUES (9, 'DSC02764.jpg', 'Un monument vu d''en bas', 3);
+-- ygjh
